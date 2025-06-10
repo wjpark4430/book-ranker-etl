c60a97318169cf93e44778be0f828c479caa46b8
woojin <pwj4na2@gmail.com>
2025-06-10 14:07:52 +0900

    테이블 변경에 따른 sql 수정

--- analysis/date_first.sql
+++ analysis/date_first.sql
@@ -1,5 +1,32 @@
 -- 날짜별 랭킹 1위 도서
-SELECT date_added, title, author
-FROM book_rank
-WHERE rank = 1
+WITH ranked_authors AS (
+    SELECT r.date_added,
+        b.title,
+        p.name AS author,
+        c.book_role,
+        ROW_NUMBER() OVER (
+            PARTITION BY r.id
+            ORDER BY CASE
+                    c.book_role
+                    WHEN '저' THEN 1
+                    WHEN '글' THEN 2
+                    WHEN '글그림' THEN 3
+                    WHEN '그림' THEN 4
+                    WHEN '역' THEN 5
+                    WHEN '감수' THEN 6
+                    ELSE 9
+                END
+        ) AS rn
+    FROM book_rank r
+        JOIN book b ON r.book_id = b.id
+        JOIN contribute c ON b.id = c.book_id
+        JOIN person p ON c.person_id = p.id
+    WHERE r.book_rank = 1
+)
+SELECT date_added,
+    title,
+    author,
+    book_role
+FROM ranked_authors
+WHERE rn = 1
 ORDER BY date_added;
--- analysis/top_author.sql
+++ analysis/top_author.sql
@@ -1,6 +1,10 @@
 -- 누적 등장 횟수가 많은 저자 TOP 5
-SELECT author, COUNT(*) AS freq
-FROM book_rank
-GROUP BY author
+SELECT p.name,
+    COUNT(*) AS freq
+FROM book_rank r
+    JOIN book b ON r.book_id = b.id
+    JOIN contribute c ON b.id = c.book_id
+    JOIN person p ON c.person_id = p.id
+GROUP BY p.name
 ORDER BY freq DESC
 LIMIT 5;
