69970c0dbfb192e094d26f37f7964f741f27db74
woojin <pwj4na2@gmail.com>
2025-06-13 21:52:46 +0900

    sh 수정

--- scheduler/daily_rank.sh
+++ scheduler/daily_rank.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+TODAY=$(date '+%Y-%m-%d')
+LOG_FILE="logs/etl_$TODAY.log"
 
 # 1. 프로젝트 디렉터리로 이동
 cd "$(dirname "$0")/.." || exit
 
 # 2. 크롤링 실행
 echo "[`date`] 크롤링 시작"
-python -u -m crawler.yes24_crawler
-python -u -m crawler.aladin_crawler
+PYTHONIOENCODING=utf-8 python -u -m crawler.yes24_crawler >> logs/yes24_etl_$TODAY.log 2>&1
+PYTHONIOENCODING=utf-8 python -u -m crawler.aladin_crawler >> logs/aladin_etl_$TODAY.log 2>&1
 
 # 3. DB 저장 실행
 echo "[`date`] DB 저장 시작"
-python -m db.save_to_db_aladin
-python -m db.save_to_db_yes24
-
-echo "[`date`] 작업 완료"
+PYTHONIOENCODING=utf-8 python -u -m db.save_to_db_aladin >> logs/yes24_etl_$TODAY.log 2>&1
+PYTHONIOENCODING=utf-8 python -u -m db.save_to_db_yes24 >> logs/aladin_etl_$TODAY.log 2>&1
