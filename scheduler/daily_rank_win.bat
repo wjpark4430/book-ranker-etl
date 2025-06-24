3e71d286d109f92e99f8577358540f5a8bb07c0b
woojin <pwj4na2@gmail.com>
2025-06-24 11:30:00 +0900

    daliy_rank 스케줄러 불가 수정

--- scheduler/daily_rank_win.bat
+++ scheduler/daily_rank_win.bat
@@ -1,2 +1,8 @@
 @echo off
+echo [INFO] Script started: %date% %time%
+cd /d %~dp0
+
 "C:\Program Files\Git\usr\bin\bash.exe" -l -c "./daily_rank.sh"
+
+echo [INFO] Script ended: %date% %time%
+pause
